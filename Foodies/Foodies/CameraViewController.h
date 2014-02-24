d46f8ab8f84b365dfb673479f6cce0c1a46a4820
Lucas Chwe <lucas.chwe@gmail.com>
2014-02-24 16:37:39 -0500

    start adding camera view

---
+++ Foodies/Foodies/CameraViewController.h
@@ -0,0 +1,13 @@
+//
+//  CameraViewController.h
+//  Foodies
+//
+//  Created by Lucas Chwe on 2/24/14.
+//  Copyright (c) 2014 Lucas Chwe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CameraViewController : UIViewController
+
+@end
