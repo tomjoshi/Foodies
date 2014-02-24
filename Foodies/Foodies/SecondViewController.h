27796acfb5f9c4b4965b002d5096c8779cf99adc
Lucas Chwe <lucas.chwe@gmail.com>
2014-02-24 12:02:31 -0500

    initial commit

---
+++ Foodies/Foodies/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  Foodies
+//
+//  Created by Lucas Chwe on 2/24/14.
+//  Copyright (c) 2014 Lucas Chwe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
