1821eebe7320362b72d9cb95c1fee78c98f0c29f
Lucas Chwe <lucas.chwe@gmail.com>
2014-02-24 12:02:31 -0500

    initial commit

---
+++ Foodies/Foodies/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Foodies
+//
+//  Created by Lucas Chwe on 2/24/14.
+//  Copyright (c) 2014 Lucas Chwe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
