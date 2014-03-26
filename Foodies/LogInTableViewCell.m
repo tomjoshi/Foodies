06d6bb3ba3748b26c38e1c62fb64d643ac6616c7
Lucas Chwe <lucas.chwe@gmail.com>
2014-03-26 15:00:37 -0400

    start working on login viewcontroller

---
+++ Foodies/LogInTableViewCell.m
@@ -0,0 +1,34 @@
+//
+//  LogInTableViewCell.m
+//  Foodies
+//
+//  Created by Lucas Chwe on 3/26/14.
+//  Copyright (c) 2014 Lucas Chwe. All rights reserved.
+//
+
+#import "LogInTableViewCell.h"
+
+@implementation LogInTableViewCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
