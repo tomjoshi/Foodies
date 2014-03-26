4afdd146fcf91c8e2d7b2a899c348ad9594a1ad9
Lucas Chwe <lucas.chwe@gmail.com>
2014-03-26 15:46:32 -0400

    add form functionality to login view

--- Foodies/LandingTableViewController.m
+++ Foodies/LandingTableViewController.m
@@ -9,11 +9,12 @@
 #import "LandingTableViewController.h"
 #import "LogInTableViewCell.h"
 
-@interface LandingTableViewController () <UITableViewDelegate, UITableViewDataSource, UIScrollViewDelegate, UITextFieldDelegate>
+@interface LandingTableViewController () <UITableViewDelegate, UITableViewDataSource, UIScrollViewDelegate, UITextFieldDelegate, UIGestureRecognizerDelegate>
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 
 
+- (IBAction)logInTapped:(id)sender;
 
 @end
 
@@ -25,6 +26,14 @@
     self.tableView.delegate = self;
     self.tableView.dataSource = self;
     self.scrollView.delegate = self;
+    
+    [self.scrollView setUserInteractionEnabled:YES];
+    
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(scrollTapped:)];
+    tap.delegate = self;
+    tap.numberOfTapsRequired = 1;
+    [self.scrollView addGestureRecognizer:tap];
+    
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
     
@@ -118,9 +127,7 @@
 
 -(void)textFieldDidBeginEditing:(UITextField *)textField
 {
-    NSLog(@"%f", self.scrollView.contentOffset.y);
-    
-    [self.scrollView setContentOffset:CGPointMake(0, 90) animated:YES];
+    [self.scrollView setContentOffset:CGPointMake(0, 80) animated:YES];
 }
 
 - (BOOL)textFieldShouldReturn:(UITextField *)textField
@@ -129,13 +136,54 @@
         LogInTableViewCell *cell = (LogInTableViewCell *)[self.tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:1 inSection:0]];
         [cell.textField becomeFirstResponder];
     } else {
-        [self.scrollView setContentOffset:CGPointMake(0, 0) animated:YES];
-        [textField resignFirstResponder];
+        [self hideKeyboard];
         
         // press login
-        // [self logInPressed:nil];
+        [self logInTapped:nil];
     }
     return YES;
 }
 
+- (void)hideKeyboard
+{
+    
+    LogInTableViewCell *cell1 = (LogInTableViewCell *)[self.tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:0]];
+    LogInTableViewCell *cell2 = (LogInTableViewCell *)[self.tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:1 inSection:0]];
+    
+    if ([cell1.textField isFirstResponder]) {
+        [cell1.textField resignFirstResponder];
+    } else {
+        [cell2.textField resignFirstResponder];
+    }
+    
+    [self.scrollView setContentOffset:CGPointMake(0, 0) animated:YES];
+}
+
+- (void)scrollTapped:(UITapGestureRecognizer *)sender
+{
+    CGPoint location = [sender locationInView:self.scrollView];
+    
+    LogInTableViewCell *cell1 = (LogInTableViewCell *)[self.tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:0]];
+    LogInTableViewCell *cell2 = (LogInTableViewCell *)[self.tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:1 inSection:0]];
+    
+    BOOL touchOnCell1 = CGRectContainsPoint(cell1.frame, location);
+    BOOL touchOnCell2 = CGRectContainsPoint(cell2.frame, location);
+    
+    if (([cell1.textField isFirstResponder] || [cell2.textField isFirstResponder]) && (!touchOnCell1 || !touchOnCell2)) {
+        [self hideKeyboard];
+    }
+
+}
+
+- (IBAction)logInTapped:(id)sender {
+    
+    // verify format of email and password
+    
+    // if its all good then hide keyboard
+    [self hideKeyboard];
+    
+    // make api call to receive user id
+    
+    // store user id in nsuserdefaults and dismiss this modal view
+}
 @end
