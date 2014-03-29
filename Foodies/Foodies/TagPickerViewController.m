cef89830a9b821cb42836edd68364d3fe3b2c7f4
Lucas Chwe <lucas.chwe@gmail.com>
2014-03-29 17:20:47 -0400

    improove camera view

---
+++ Foodies/Foodies/TagPickerViewController.m
@@ -0,0 +1,52 @@
+//
+//  TagPickerViewController.m
+//  Foodies
+//
+//  Created by Lucas Chwe on 3/29/14.
+//  Copyright (c) 2014 Lucas Chwe. All rights reserved.
+//
+
+#import "TagPickerViewController.h"
+#import "MealTag.h"
+
+@interface TagPickerViewController ()
+@property (strong, nonatomic) NSMutableArray *mealTags;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@end
+
+@implementation TagPickerViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
