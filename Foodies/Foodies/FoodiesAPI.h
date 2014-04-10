9d3d3ac69407a882674b64626fb0f0dd3f5f5396
Lucas Chwe <lucas.chwe@gmail.com>
2014-04-10 12:13:35 -0400

    Reorganize API for log in stuff. Get started with API call make a food post

---
+++ Foodies/Foodies/FoodiesAPI.h
@@ -0,0 +1,20 @@
+//
+//  FoodiesAPI.h
+//  Foodies
+//
+//  Created by Lucas Chwe on 4/10/14.
+//  Copyright (c) 2014 Lucas Chwe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Parse/Parse.h>
+#import "FoodPost.h"
+
+@interface FoodiesAPI : NSObject
+// FoodPost related
++ (void)postFoodPost:(FoodPost *)newFoodPost;
+
+// Foodie related
++ (void)signUpWithUsernameInBackground:(NSString *)username password:(NSString *)password email:(NSString *)email success:(void (^)(void))successBlock failure:(void (^)(NSError *error))failureBlock;
++ (void)logInWithUsernameInBackground:(NSString *)username password:(NSString *)password success:(void (^)(void))successBlock failure:(void (^)(NSError *error))failureBlock;
+@end
